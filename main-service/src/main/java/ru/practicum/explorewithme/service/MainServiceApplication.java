da965581798a3f9f910e85832f329e5a3b202414
Анастасия Нагайцева <nagaitzeva.anastasia@yandex.ru>
2026-01-12 17:04:38 +0300

    Исправлены документы.

--- main-service/src/main/java/ru/practicum/explorewithme/service/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/explorewithme/service/MainServiceApplication.java
@@ -2,8 +2,13 @@ package ru.practicum.explorewithme.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "ru.practicum.explorewithme.service",
+        "ru.practicum.explorewithme.client"
+})
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
