4265c6cad6b9c0b6668c5413d425527ded5a4b8f
Анастасия Нагайцева <nagaitzeva.anastasia@yandex.ru>
2026-01-14 12:56:06 +0300

    Исправление ошибок

--- main-service/src/main/java/ru/practicum/explorewithme/service/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/explorewithme/service/repository/EventRepository.java
@@ -68,22 +68,28 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             "LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')))")
     Page<Event> findPublishedEventsWithText(@Param("text") String text, Pageable pageable);
 
-    @Query("SELECT e FROM Event e " +
-            "WHERE e.state = 'PUBLISHED' " +
-            "AND (:text IS NULL OR :text = '' OR " +
-            "     LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
-            "     LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-            "AND (:categories IS NULL OR e.category.id IN :categories) " +
-            "AND (:paid IS NULL OR e.paid = :paid) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) " +
-            "AND (:onlyAvailable = false OR e.participantLimit = 0 OR e.confirmedRequests < e.participantLimit)")
-    Page<Event> findPublicEvents(
+    @Query(value = """
+    SELECT e.* FROM events e\s
+    WHERE e.state = 'PUBLISHED'\s
+    AND (:text IS NULL OR :text = '' OR\s
+         LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR\s
+         LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')))\s
+    AND (:categories IS NULL OR e.category_id IN (:categories))\s
+    AND (:paid IS NULL OR e.paid = CAST(:paid AS BOOLEAN))\s
+    AND (:rangeStart IS NULL OR e.event_date >= :rangeStart)\s
+    AND (:rangeEnd IS NULL OR e.event_date <= :rangeEnd)\s
+    AND (:onlyAvailable = false OR e.participant_limit = 0 OR e.confirmed_requests < e.participant_limit)
+    ORDER BY e.event_date\s
+    OFFSET :offset ROWS FETCH NEXT :size ROWS ONLY
+   \s""",
+            nativeQuery = true)
+    List<Event> findPublicEvents(
             @Param("text") String text,
             @Param("categories") List<Long> categories,
             @Param("paid") Boolean paid,
             @Param("rangeStart") LocalDateTime rangeStart,
             @Param("rangeEnd") LocalDateTime rangeEnd,
             @Param("onlyAvailable") Boolean onlyAvailable,
-            Pageable pageable);
+            @Param("offset") int offset,
+            @Param("size") int size);
 }
